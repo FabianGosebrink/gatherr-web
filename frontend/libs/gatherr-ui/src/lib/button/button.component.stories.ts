02b9a982e07674276b70f576bd15ad61fe29b889
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-04-06 20:56:01 +0200

    chore: setup stories for button component

---
+++ frontend/libs/gatherr-ui/src/lib/button/button.component.stories.ts
@@ -0,0 +1,16 @@
+import { Story } from '@storybook/angular';
+import { text } from '@storybook/addon-knobs';
+
+import { GatherrUiModule } from '../gatherr-ui.module';
+
+export default { title: '<grr-button>' };
+
+export const usage: Story = () => ({
+  moduleMetadata: { imports: [GatherrUiModule] },
+  template: `
+    <grr-button>{{ caption }}</grr-button>
+  `,
+  props: {
+    caption: text('Caption', 'Click here to continue'),
+  },
+});
