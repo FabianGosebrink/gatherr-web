d2a3d3c2d416384b878435dd9cd20d031dfdab03
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-04-25 22:20:29 +0200

    chore: setup viewports for stories

--- frontend/libs/shared/ui-common/.storybook/preview.js
+++ frontend/libs/shared/ui-common/.storybook/preview.js
@@ -1,4 +1,21 @@
-import { addDecorator } from '@storybook/angular';
 import { withKnobs } from '@storybook/addon-knobs';
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { addDecorator } from '@storybook/angular';
 
 addDecorator(withKnobs);
+
+export const parameters = {
+  viewport: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      custom: {
+        name: 'Custom',
+        styles: {
+          width: '650px',
+          height: '450px',
+        },
+      },
+    },
+    defaultViewport: 'custom',
+  },
+};
